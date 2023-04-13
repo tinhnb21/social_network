2da0ee2f9dd0366cdc554ab765317b3d717df109
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2023-04-13 15:56:47 +0700

    TinhNB commit

---
+++ src/modules/posts/posts.model.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import { IPost } from "./posts.interface";
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model<IPost & mongoose.Document>("Post", PostSchema);
